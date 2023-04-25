42eee75dc3873cbf8d77b6fa18fbf3144b3ac00b
Brett Evans <evansbrett86@gmail.com>
2023-04-24 22:47:05 -0400

    doc params and views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,14 +5,23 @@ class DocsController < ApplicationController
 
   def show
   end
-  
+
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   def edit
+    
   end
 
   def update
@@ -27,5 +36,6 @@ class DocsController < ApplicationController
     end
 
     def doc_params
+      params.require(:doc).permit(:title, :content)
     end
 end
